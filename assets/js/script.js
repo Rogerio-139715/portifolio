198fd7a0d13b3f334d45d6fe15a538ee794d0b2e
Rogerio Martins <rmaviador@gmail.com>
2020-10-27 21:16:59 -0300

    git graph

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,33 @@
-
-
-
-var hamburguer = document.querySelector(".hamburguer").addEventListener( "click", () => 
+var hamburguer = document.querySelector(".hamburguer").addEventListener("click", () =>
    document.querySelector(".container").classList.toggle("show-menu")
 );
 
-document.querySelector("#qtde").addEventListener("change",  atualizarPreco);
-document.querySelector("#js").addEventListener("change",  atualizarPreco);
-document.querySelector("#layout-sim").addEventListener("change",  atualizarPreco);
-document.querySelector("#layout-nao").addEventListener("change",  atualizarPreco);
-document.querySelector("#prazo").addEventListener("change", function (){
-   
-  
-  const prazo = document.querySelector("#prazo").value
-   document.querySelector("label[for=prazo]").innerHTML = `prazo: ${prazo} semanas`
-   atualizarPreco()
-} );
+document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+document.querySelector("#js").addEventListener("change", atualizarPreco);
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco);
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco);
+document.querySelector("#prazo").addEventListener("change", function () {
 
-function atualizarPreco () {
-   const qtde = document.querySelector("#qtde").value 
+
+   const prazo = document.querySelector("#prazo").value
+   if (prazo > 1) {
+      document.querySelector("label[for=prazo]").innerHTML = `prazo: ${prazo} semanas`
+   } else {
+      document.querySelector("label[for=prazo]").innerHTML = `prazo: ${prazo} semana`
+   }
+   
+   atualizarPreco()
+});
+
+function atualizarPreco() {
+   const qtde = document.querySelector("#qtde").value
    const temJs = document.querySelector("#js").checked
    const incluiLayout = document.querySelector("#layout-sim").checked
-   const prazo = document.querySelector("#prazo").value 
+   const prazo = document.querySelector("#prazo").value
    let preco = qtde * 100;
-   if(temJs) preco *=  1.1
-   if(incluiLayout) preco += 500
-   let taxaUrgencia = 1 - prazo*0.1;
+   if (temJs) preco *= 1.1
+   if (incluiLayout) preco += 500
+   let taxaUrgencia = 1 - prazo * 0.1;
    preco *= 1 + taxaUrgencia
 
    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
